f8a0f5e631e1bc34de37ec15d322ad60b8134072
nikmeiser <nikmeiser@NIKHILs-MacBook-Pro.local>
2014-07-08 11:45:50 -0400

    rec.js

--- rec.js
+++ rec.js
@@ -19,16 +19,29 @@ Subsets.prototype.subsetize = function(superset, subset, subsetsize, nextindex){
 }
 
 Subsets.prototype.getSubsets = function(superset, subsetsize) {
-	for(i=0;i<superset.length;i+=1) {
-		subset = new Array(subsetsize)
-		this.subsetize(superset,subset, 0, i)
+	if (superset.length < 1) {
+		return new Error("SupersetSizeInvalid")
+	} else if (subsetsize < 0) {
+		return new Error("NoNegativeSubset")
+	} else if (superset.length < subsetsize){
+		return new Error("SupersetSizeTooShort")
+	} else if (superset.length == 1){
+		this.merge(superset)
+	} else if (subsetsize < 2) {
+		return new Error("SubsetSizeTooShort")
+	} else {
+		for(i=0;i<superset.length;i+=1) {
+			subset = new Array(subsetsize)
+			this.subsetize(superset,subset, 0, i)
+		}
+		return this.retarr
 	}
-	return this.retarr
 }
 
 Subsets.prototype.retarr = []
 
 arr = [1,2,3,4,5,6,7,8,9]
+arr2 = [1]
 s = new Subsets()
-x = s.getSubsets(arr,5)
+x = s.getSubsets(arr,1)
 console.log(x)

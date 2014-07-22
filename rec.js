395168bdf7aa60c4a81961a411423f52b91a4823
nikmeiser <nikmeiser@NIKHILs-MacBook-Pro.local>
2014-07-21 20:58:37 -0400

    scope bug

--- rec.js
+++ rec.js
@@ -1,6 +1,6 @@
 
 
-Subsets = function(){}
+Subsets = function(){this.retarr = []}
 
 Subsets.prototype.merge = function(subset){
 	//console.log(subset)
@@ -36,7 +36,7 @@ Subsets.prototype.getSubsets = function(superset, subsetsize) {
 	}
 }
 
-Subsets.prototype.retarr = []
+//Subsets.prototype.retarr = []
 
 arr = [1,2,3,4,5,6,7,8,9]
 arr2 = [1]
